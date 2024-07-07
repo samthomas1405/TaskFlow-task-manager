03c22929e45e65b7b03aa79aa32157501bd4fa77
Samuel Thomas <samthomas1405@gmail.com>
2024-07-07 17:37:08 -0400

    First commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {MdOutlineSearch } from "react-icons/md"
+import { useSelector } from 'react-redux'
+
+const Navbar = () => {
+    const { user } = useSelector ((state) => state.auth);
+    const dispatch = useDispatch();
+  return (
+    <div className = 'flex justify-between items-center bg-white px-4 py-3 2xl:py-4 sticky z-10 top-0'>
+        <div className = 'flex gap-4'>
+            <button className = 'text-2xl text-gray-500 block md:hidden'>☰</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
