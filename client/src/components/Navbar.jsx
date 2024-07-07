40036443a37f30bd9b193f9dc6627ae627dd60e3
Samuel Thomas <samthomas1405@gmail.com>
2024-07-07 18:38:52 -0400

    Update button in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,8 @@ const Navbar = () => {
   return (
     <div className = 'flex justify-between items-center bg-white px-4 py-3 2xl:py-4 sticky z-10 top-0'>
         <div className = 'flex gap-4'>
-            <button className = 'text-2xl text-gray-500 block md:hidden'>☰</button>
+            <button onClick = {() => dispatch(setOpenSidebar(true))}
+            className = 'text-2xl text-gray-500 block md:hidden'>☰</button>
         </div>
     </div>
   )
