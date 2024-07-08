20c725fafc37fde57498166210581dd07a2d1b99
Samuel Thomas <samthomas1405@gmail.com>
2024-07-08 11:32:05 -0400

    Completed NavBar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {MdOutlineSearch } from "react-icons/md"
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { setOpenSidebar } from "../redux/slices/authSlice";
+import UserAvatar from './UserAvatar';
+import NotificationPanel from './NotificationPanel';
 
 const Navbar = () => {
     const { user } = useSelector ((state) => state.auth);
@@ -10,6 +13,21 @@ const Navbar = () => {
         <div className = 'flex gap-4'>
             <button onClick = {() => dispatch(setOpenSidebar(true))}
             className = 'text-2xl text-gray-500 block md:hidden'>☰</button>
+
+            <div className = 'w-64 2xl:w-[400px] flex items-center py-2 px-3 gap-2 rounded-full bg-[#f3f4f6]'>
+              <MdOutlineSearch className = 'text-gray-500 text-xl' />
+              <input 
+                type = "text"
+                placeholder = "Search..."
+                className = 'flex-1 outline-none bg-transparent placeholder:text-gray-500 text-gray-800'
+              />
+            </div>
+        </div>
+
+        <div className = 'flex gap-2 items-center'>
+          < NotificationPanel />
+
+          < UserAvatar />
         </div>
     </div>
   )
